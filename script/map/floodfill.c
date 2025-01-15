852e6fbd6c0f0607528139179b16d489b6e3d4eb
emaillet <maillet.emilien@gmail.com>
2025-01-15 08:22:32 +0100

    FloodFill_StepUp

--- script/map/floodfill.c
+++ script/map/floodfill.c
@@ -6,7 +6,7 @@
 /*   By: emaillet <emaillet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 06:02:46 by emaillet          #+#    #+#             */
-/*   Updated: 2025/01/15 08:16:23 by emaillet         ###   ########.fr       */
+/*   Updated: 2025/01/15 08:22:21 by emaillet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,56 +48,60 @@ void	maprint(char **tab)
 	}
 }
 
-int floodfill_collect(t_mlx_data *data, int x, int y)
+int	floodfill_collect(t_mlx_data *data, int x, int y)
 {
 	ft_printf("\n\nFirst Flood = x %i y %i \n", x, y);
-    maprint(data->floodfill->map);
-    if (x < 0 || x >= data->map->size_x || y < 0 || y >= data->map->size_y)
-        return (0);
-    if (data->floodfill->map[y][x] == M_WALL
+	maprint(data->floodfill->map);
+	if (x < 0 || x >= data->map->size_x || y < 0 || y >= data->map->size_y)
+		return (0);
+	if (data->floodfill->map[y][x] == M_WALL
 		|| data->floodfill->map[y][x] == M_FLOOD
 		|| data->floodfill->map[y][x] == M_DOOR_OBJ_C)
-        return (0);
-    if (data->floodfill->map[y][x] == M_CH_OBJ_C)
-        data->floodfill->obj++;
-    data->floodfill->map[y][x] = M_FLOOD;
-    floodfill_collect(data, x + 1, y);
-    floodfill_collect(data, x - 1, y);
-    floodfill_collect(data, x, y + 1);
-    floodfill_collect(data, x, y - 1);
-    return (1);
+		return (0);
+	if (data->floodfill->map[y][x] == M_CH_OBJ_C)
+		data->floodfill->obj++;
+	data->floodfill->map[y][x] = M_FLOOD;
+	floodfill_collect(data, x + 1, y);
+	floodfill_collect(data, x - 1, y);
+	floodfill_collect(data, x, y + 1);
+	floodfill_collect(data, x, y - 1);
+	return (1);
 }
 
-int floodfill_reach(t_mlx_data *data, int x, int y)
+int	floodfill_reach(t_mlx_data *data, int x, int y)
 {
 	ft_printf("\n\nSecond Flood = x %i y %i \n", x, y);
-    maprint(data->floodfill->map);
-    if (x < 0 || x >= data->map->size_x || y < 0 || y >= data->map->size_y)
-        return (0);
-    if (data->floodfill->map[y][x] == M_WALL || data->floodfill->map[y][x] == M_FLOOD2 ||
-        (data->floodfill->map[y][x] == M_DOOR_OBJ_C && data->floodfill->obj < data->map->obj))
-        return (0);
-    if (data->floodfill->map[y][x] == M_FINISH && data->floodfill->obj == data->map->obj)
-        return (1);
-    data->floodfill->map[y][x] = M_FLOOD2;
-    if (floodfill_reach(data, x + 1, y)
-        || floodfill_reach(data, x - 1, y)
-        || floodfill_reach(data, x, y + 1)
-        || floodfill_reach(data, x, y - 1))
-        return (1);
-
-    return (0);
+	maprint(data->floodfill->map);
+	if (x < 0 || x >= data->map->size_x || y < 0 || y >= data->map->size_y)
+		return (0);
+	if (data->floodfill->map[y][x] == M_WALL
+		|| data->floodfill->map[y][x] == M_FLOOD2 ||
+		(data->floodfill->map[y][x] == M_DOOR_OBJ_C
+		&& data->floodfill->obj < data->map->obj))
+		return (0);
+	if (data->floodfill->map[y][x] == M_FINISH
+		&& data->floodfill->obj == data->map->obj)
+		return (1);
+	data->floodfill->map[y][x] = M_FLOOD2;
+	if (floodfill_reach(data, x + 1, y)
+		|| floodfill_reach(data, x - 1, y)
+		|| floodfill_reach(data, x, y + 1)
+		|| floodfill_reach(data, x, y - 1))
+		return (1);
+	return (0);
 }
 
-int floodfill(t_mlx_data *data, int x, int y)
+int	floodfill(t_mlx_data *data, int x, int y)
 {
-    floodfill_collect(data, x, y);
+	floodfill_collect(data, x, y);
 	if (!floodfill_reach(data, x, y))
 	{
 		if (data->floodfill->obj != data->map->obj)
-			ft_printf(YEL"[WARNING]\nFirst floodfill cant get all objectives...\n"RES);
-		ft_printf(YEL"[WARNING]\nSecond floodfill cant reach the end of map...\n"RES);
+			ft_printf(YEL
+				"[WARNING]\nFirst floodfill cant get all objectives...\n"RES);
+		ft_printf(YEL
+			"[WARNING]\nSecond floodfill cant reach the end of map...\n"RES);
 		return (0);
 	}
-    return (1);
+	return (1);
 }
